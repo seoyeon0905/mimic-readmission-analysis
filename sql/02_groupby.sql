28d5edf4b973726e3a0a8855418cf1203f9bfecb
seoyeoning <ssseoyeon95@naver.com>
2026-01-20 13:02:08 +0900

    Delete sql directory

--- sql/02_groupby.sql
+++
@@ -1,8 +0,0 @@
--- 성별 환자 수 집계
-SELECT gender, COUNT(*) AS patient_count
-FROM patients
-GROUP BY gender;
-
--- 전체 환자 평균 나이
-SELECT AVG(age) AS avg_age
-FROM patients;
--- sql/README.md
+++
@@ -1,5 +0,0 @@
-# SQL Practice
-
-This folder contains basic SQL queries for healthcare-style datasets,
-including SELECT, GROUP BY, and JOIN operations.
-
